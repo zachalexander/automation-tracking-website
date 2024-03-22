ee1e1b97292cbeac1a0db54c3ac2483003abdcba
zachalexander <alexander.d.zachary@gmail.com>
2024-03-22 15:43:47 -0400

    adds auth module

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [AmplifyAuthenticatorModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
